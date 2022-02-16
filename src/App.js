c78cfe55599314c3bb393ec8e585decde69d2fcb
Lucas Txai <txaifonceca@gmail.com>
2022-02-15 22:08:45 -0300

    começamos a página inicial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Router from './Routes/Router';
 import { BrowserRouter } from 'react-router-dom';
-
 import { ChakraProvider } from '@chakra-ui/react'
 // import Loading from './constants/Loading';
 
 function App() {
   return (
     <ChakraProvider>
-
       <BrowserRouter>
         <Router />
       </BrowserRouter>
--- src/constants/Loading.js
+++ src/constants/Loading.js
@@ -1,14 +1,14 @@
-// import { Container, Center, Image } from '@chakra-ui/react'
-// import logo from "../assets/logo.png"
+import { Container, Center, Image } from '@chakra-ui/react'
+import logo from "../../assets/logo-branca.png"
 
 
-// export default function Loading() {
+export default function HomePage() {
 
-//     return (
-//         <Container w={"100vw"} h={"100vh"} bg={"#e86e5a"}>
-//             <Center>
-//                 <Image src={logo}/>
-//             </Center>
-//         </Container>
-//     )
-// }
+    return (
+        <Container w={"100vw"} h={"100vh"} bg={"#e86e5a"}>
+            <Center>
+                <Image src={logo}/>
+            </Center>
+        </Container>
+    )
+}
