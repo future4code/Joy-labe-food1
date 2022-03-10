008c070f55cadd9010cd1ca693db29dc143ee524
Laura Lanna <laura.lannab@gmail.com>
2022-03-09 22:17:35 -0300

    Fiz as páginas do perfil, editar endereço e perfil, e algumas correções

--- src/pages/EditProfilePage/EditProfilePage.js
+++ src/pages/EditProfilePage/EditProfilePage.js
@@ -1,10 +1,174 @@
 import React from "react";
+import axios from "axios";
+import useForm from "../../hooks/useForm";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../../constants/BASE_URL";
+import { goToHome, goToProfilePage } from "../../Routes/Coordinator";
+import {
+  Center,
+  ChakraProvider,
+  FormControl,
+  FormLabel,
+  Input,
+  extendTheme,
+  Grid,
+  GridItem,
+  Image,
+  Text,
+  Button,
+  Container,
+} from "@chakra-ui/react";
+import back from "../../assets/back.svg";
+import logo from "../../assets/logo-laranja.png";
+import { auth } from "../../constants/auth";
+
+const activeLabelStyles = {
+  transform: "scale(0.85) translateY(-24px) translateX(-10px)",
+};
+export const theme = extendTheme({
+    components: {
+      Form: {
+        variants: {
+          floating: {
+            container: {
+              right:"12px",
+              _focusWithin: {
+                label: {
+                  ...activeLabelStyles,
+                },
+              },
+              "input:not(:placeholder-shown) + label, .chakra-select__wrapper + label":
+                {
+                  ...activeLabelStyles,
+                },
+              label: {
+                top: "-9px",
+                left: "10px",
+                zIndex: 2,
+                position: "absolute",
+                backgroundColor: "white",
+                pointerEvents: "none",
+                color: "#d0d0d0",
+                mx: 3,
+                px: 1,
+                my: 2,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
 
 export default function EditProfilePage(){
-    return(
-        <div>
-            <h1>EditProfilePage</h1>
-        </div>
-    )
-
+    const navigate = useNavigate();
+    const { form, onChange, cleanFields } = useForm({
+      name: "",
+      email: "",
+      cpf: "",
+    });
+  
+    const onSubmitEditProfile = (event) => {
+      event.preventDefault();
+      const body = {
+        name: form.name,
+        email: form.email,
+        cpf: form.cpf,
+      };
+      axios
+        .put(`${BASE_URL}/profile`, body, auth)
+        .then((res) => {
+          cleanFields();
+          console.log(res.data);
+          window.alert("Perfil editado com sucesso!");
+          goToProfilePage(navigate);
+        })
+        .catch((err) => {
+          window.alert(err.response.data.message);
+        });
+    };
+  
+    return (
+      <Container padding={0}>
+        <Grid>
+          <Image
+            src={back}
+            margin={"10px"}
+            onClick={() => goToProfilePage(navigate)}
+          />
+        </Grid>
+        <hr />
+        <Center>
+          <ChakraProvider theme={theme}>
+            <Grid templateRows="1fr 8fr">
+              <GridItem>
+                <Center>
+                  <Text fontSize="16px" mt="24px">Editar</Text>
+                </Center>
+              </GridItem>
+              <GridItem w="328px">
+                <FormControl variant="floating" id="name" isRequired isInvalid>
+                  <FormLabel htmlFor="name">Nome</FormLabel>
+                  <Input
+                    h="56px"
+                    id="name"
+                    type={"name"}
+                    value={form.name}
+                    onChange={onChange}
+                    name={"name"}
+                    placeholder={"Nome"}
+                    m="3"
+                    required
+                  />
+                </FormControl>
+                <br />
+  
+                <FormControl variant="floating" id="email" isRequired isInvalid>
+                  <FormLabel htmlFor="email">E-mail</FormLabel>
+                  <Input
+                    h="56px"
+                    id="email"
+                    type={"email"}
+                    value={form.email}
+                    onChange={onChange}
+                    name={"email"}
+                    placeholder={"E-mail"}
+                    m="3"
+                    required
+                  />
+                </FormControl>
+                <br />
+  
+                <FormControl variant="floating" id="cpf" isInvalid>
+                  <FormLabel htmlFor="cpf">CPF</FormLabel>
+                  <Input
+                    h="56px"
+                    id="cpf"
+                    type={"cpf"}
+                    value={form.cpf}
+                    onChange={onChange}
+                    name={"cpf"}
+                    placeholder={"000.000.000-00"}
+                    m="3"
+                  />
+                </FormControl>
+                <br />
+  
+                <Button
+                  // marginLeft="14px"
+                  bg="#e86e5a"
+                  w="328px"
+                  h="42px"
+                  type={"submit"}
+                  onClick={onSubmitEditProfile}
+                >
+                  Salvar
+                </Button>
+              </GridItem>
+            </Grid>
+          </ChakraProvider>
+        </Center>
+      </Container>
+    );
+  
 }
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,10 +1,208 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../constants/BASE_URL";
+import RestaurantCard from "../../components/RestaurantsCard/RestaurantsCard";
+import {
+  Box,
+  Center,
+  Divider,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Spinner,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
+import { AiOutlineHome, AiOutlineShoppingCart } from "react-icons/ai";
+import { MdPersonOutline } from "react-icons/md";
+import {
+  goToCartPage,
+  goToHome,
+  goToProfilePage,
+  goToResultPage,
+} from "../../Routes/Coordinator";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../constants/auth";
+import { useRequestData } from "../../hooks/useRequestData";
 
-export default function ProfilePage(){
-    return(
-        <div>
-            <h1>ProfilePage</h1>
-        </div>
-    )
+export default function ProfilePage() {
+  const navigate = useNavigate();
+  //   const [users, isLoading, error] = useRequestData(`${BASE_URL}/profile`);
+  const [users, setUsers] = useState({});
+  const [orders, setOrders] = useState([]);
 
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  const getUsers = () => {
+    axios
+      .get(`${BASE_URL}/profile`, auth)
+      .then((res) => {
+        setUsers(res.data.user);
+      })
+      .catch((err) => {
+        window.alert(err);
+      });
+  };
+  const getOrders = () => {
+    axios
+      .get(`${BASE_URL}/orders/history`, auth)
+      .then((res) => {
+        setOrders(res.data.orders);
+        console.log(res.data.orders);
+      })
+      .catch((err) => {
+        window.alert(err);
+      });
+  };
+
+  const ordersDate = orders.map((order) => {
+    const date = new Date(order.createdAt);
+    const options = { day: "numeric", month: "long", year: "numeric" };
+
+    return (
+      <Flex
+        borderWidth="1px"
+        borderColor="#b8b8b8"
+        m="8px 16px"
+        borderRadius="8px"
+        p="16px"
+        direction="column"
+      >
+        <Text marginBottom="9px" fontSize="16px" color="#e86e5a">
+          {order.restaurantName}
+        </Text>
+        <Text marginBottom="7px" fontSize="12px">
+          {date.toLocaleDateString("pt-br", options)}
+        </Text>
+        <Text fontSize="16px" fontWeight="bold">
+          SUBTOTAL R${order.totalPrice.toFixed(2).replace(".", ",")}
+        </Text>
+      </Flex>
+    );
+  });
+
+  return (
+    <Grid templateRows="1fr 2fr 1fr" maxH="100vh" h="100vh">
+      <GridItem>
+        <Center>
+          <Heading
+            as="h3"
+            size={"16px"}
+            borderBottom="1px"
+            borderColor="gray.200"
+            w="328px"
+            h="44px"
+            textAlign={"center"}
+            marginBottom="8px"
+            mt="13px"
+          >
+            Meu perfil
+          </Heading>
+        </Center>
+      </GridItem>
+      <Box>
+        <GridItem>
+          <Grid>
+            <Text fontSize="16px" m="16px 16px 8px 16px">
+              {users.name}
+            </Text>
+            <Text fontSize="16px" m="0px 16px 8px 16px">
+              {users.email}
+            </Text>
+            <Text fontSize="16px" m="0px 16px 16px 16px">
+              {users.cpf}
+            </Text>
+          </Grid>
+        </GridItem>
+        <GridItem>
+          <Grid bg={"#eeeeee"}>
+            <Text m="16px 16px 8px 16px" color={"#b8b8b8"} fontSize="16px">
+              Endereço Cadastrado
+            </Text>
+            <Text m="0px 16px 16px 16px" fontSize="16px">
+              {users.address}
+            </Text>
+          </Grid>
+        </GridItem>
+        <GridItem overflowY="hidden">
+          <Text
+            alignSelf="center"
+            textAlign="left"
+            fontSize="16px"
+            m={"16px 16px 8px 16px"}
+            borderBottom="1px"
+            borderBottomColor="#000000"
+          >
+            Histórico de pedidos
+          </Text>
+          {ordersDate}
+        </GridItem>
+      </Box>
+      <GridItem>
+        <Center>
+          <Grid
+            templateColumns={"repeat(3, 1fr)"}
+            shadow={"lg"}
+            borderWidth="1px"
+            w={"100vw"}
+            align="center"
+            position={"relative"}
+            bottom="0"
+            boxShadow={
+              "0 -1px 3px 0 rgba(0, 0, 0, 0.2), 0 -2px 1px -1px rgba(0, 0, 0, 0.12), 0 -1px 1px 0 rgba(0, 0, 0, 0.14);"
+            }
+            h="49px"
+            bgColor="#FFF"
+            p="10px"
+          >
+            <GridItem>
+              <IconButton
+                as={AiOutlineHome}
+                bg={"white"}
+                w={"27px"}
+                h={"27px"}
+                color={"#e865a"}
+                onClick={() => goToHome(navigate)}
+              />
+            </GridItem>
+            <GridItem>
+              <IconButton
+                as={AiOutlineShoppingCart}
+                bg={"white"}
+                w={"27px"}
+                h={"27px"}
+                onClick={() => goToCartPage(navigate)}
+              />
+            </GridItem>
+            <GridItem>
+              <IconButton
+                as={MdPersonOutline}
+                bg={"white"}
+                w={"27px"}
+                h={"27px"}
+                onClick={() => goToProfilePage(navigate)}
+              />
+            </GridItem>
+          </Grid>
+        </Center>
+      </GridItem>
+    </Grid>
+  );
 }
