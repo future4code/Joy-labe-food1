9f4bbbe263ff8376bbb21dcd4de7800635d580b2
Lucas Txai <txaifonceca@gmail.com>
2022-03-13 13:47:12 -0300

    alteramos a fonte, criamos um novo card de produtos e iniciamos a função de adicionar quantidades

--- src/constants/GlobalStyle.js
+++ src/constants/GlobalStyle.js
@@ -2,8 +2,8 @@ import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
   fonts: {
-    heading: 'Roboto-Regular',
-    body: 'Roboto-Regular',
+    heading: 'Roboto, sans-serif',
+    body: 'Roboto, sans-serif',
   },
 })
 
